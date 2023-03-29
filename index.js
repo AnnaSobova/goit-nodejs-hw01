5c410666482d34d7727d432c6dc0b16569c390e1
Anna Sobova <anna_sobova@ukr.net>
2023-03-30 00:42:42 +0300

    with files

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-Welcome
+const contacts = require("./contacts");
+
+const yargs = require("yargs");
+const { hideBin } = require('yargs/helpers')
+
+
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts()
+      console.table(allContacts);
+      break;
+
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      console.log(oneContact);
+      break;
+
+    case "add":
+     const newContact = await contacts.addContact({name, email, phone});
+     console.log(newContact);
+     break;
+
+    case "remove":
+      const removeContact = await contacts.removeContact(id);
+      console.log(removeContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+const arr = hideBin(process.argv);
+const { argv } = yargs(arr);
+invokeAction(argv);
